1658ccbddb942f56180ed9aba73557151e06435f
pisces26 <pranjali.dixit15.sd@gmail.com>
2025-05-05 23:36:59 +0530

    hoisting for let and const

--- hoisting/let&const.js
+++ hoisting/let&const.js
@@ -10,3 +10,8 @@ var b=100;
 const c=200;
 // const d;
 // d=300; //SyntaxError: Missing initializer in const declaration
+
+
+//let and const are hoisted but are in temporal dead zone (TDZ) until they are declared
+//TDZ is the time between the start of the block and the declaration of the variable
+//in TDZ, the variable cannot be accessed, and trying to do so will result in a ReferenceError
